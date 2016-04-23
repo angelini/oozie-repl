d298223f294dc578997c2dee25f10fd3b824fed1
Alex Angelini <alex.louis.angelini@gmail.com>
2016-04-23 12:19:25 -0400

    Add repl.by_name

--- repl.py
+++ repl.py
@@ -17,21 +17,25 @@ def take(generator, n=5):
     return tuple(itertools.islice(generator, n))
 
 
-def _get_flows(user, status, n):
-    workflows = take(api.get_workflows({'user': user, 'status': status}), n)
+def _get_flows(user=None, status=None, name=None, n=5):
+    workflows = take(api.get_workflows({'user': user, 'status': status, 'name': name}), n)
     return [Flow.from_workflow_id(w['id']) for w in workflows]
 
 
 def failed(user='oozie', n=5):
-    return _get_flows(user, 'FAILED', n)
+    return _get_flows(user=user, status='FAILED', n=n)
 
 
 def running(user='oozie', n=5):
-    return _get_flows(user, 'RUNNING', n)
+    return _get_flows(user=user, status='RUNNING', n=n)
 
 
 def all(user='oozie', n=5):
-    return _get_flows(user, None, n)
+    return _get_flows(user=user, n=n)
+
+
+def by_name(name, user=None, status=None, n=5):
+    return _get_flows(user=user, status=status, name=name, n=n)
 
 
 def open_graph(workflow):
