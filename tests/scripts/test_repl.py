00eb11a7de42f4c4683132be3299458ab44f9468
Chris Fournier <chris.fournier@shopify.com>
2016-05-07 22:44:16 -0400

    Refactored tests

--- tests/scripts/test_repl.py
+++ tests/scripts/test_repl.py
@@ -5,9 +5,8 @@ from oozierepl.scripts.repl import by_name, Coordinator, Workflow
 
 
 @responses.activate
-def test_by_name_workflow(flow_name, workflows_body, oozie_host, monkeypatch):
+def test_by_name_workflow(flow_name, workflows_body, workflows, oozie_host, monkeypatch):
     monkeypatch.setenv('OOZIE_HOST', oozie_host)
-
     responses.add(
         responses.GET,
         'http://oozie-host/oozie/v2/jobs?len=50&filter=name%3Dreportify-rdb-landing-pages-daily-rollup&jobtype=wf&timezone=EST&offset=1',
@@ -15,53 +14,12 @@ def test_by_name_workflow(flow_name, workflows_body, oozie_host, monkeypatch):
         status=200,
         match_querystring=True,
     )
-    assert by_name(flow_name, form=Workflow) == [
-        WorkflowObject(
-            '0145308-160503113641936-oozie-oozi-W',
-            'reportify-rdb-landing-pages-daily-rollup',
-            'oozie',
-            'RUNNING',
-            'Sat, 07 May 2016 11:29:08 EST',
-            None
-        ),
-        WorkflowObject(
-            '0145241-160503113641960-oozie-oozi-W',
-            'reportify-rdb-landing-pages-daily-rollup',
-            'oozie',
-            'SUCCEEDED',
-            'Sat, 07 May 2016 10:05:32 EST',
-            'Sat, 07 May 2016 11:29:08 EST'
-        ),
-        WorkflowObject(
-            '0145122-160503113641936-oozie-oozi-W',
-            'reportify-rdb-landing-pages-daily-rollup',
-            'oozie',
-            'SUCCEEDED',
-            'Sat, 07 May 2016 08:29:06 EST',
-            'Sat, 07 May 2016 10:05:32 EST'
-        ),
-        WorkflowObject(
-            '0144994-160503113641936-oozie-oozi-W',
-            'reportify-rdb-landing-pages-daily-rollup',
-            'oozie',
-            'SUCCEEDED',
-            'Sat, 07 May 2016 06:55:33 EST',
-            'Sat, 07 May 2016 08:29:06 EST'
-        ),
-        WorkflowObject(
-            '0144892-160503113641936-oozie-oozi-W',
-            'reportify-rdb-landing-pages-daily-rollup',
-            'oozie',
-            'SUCCEEDED',
-            'Sat, 07 May 2016 04:54:14 EST',
-            'Sat, 07 May 2016 06:55:33 EST'
-        ),
-    ]
+    assert by_name(flow_name, form=Workflow) == workflows
+
 
 @responses.activate
-def test_by_name_coordinator(flow_name, coordinators_body, oozie_host, monkeypatch):
+def test_by_name_coordinator(flow_name, coordinators_body, coordinators, oozie_host, monkeypatch):
     monkeypatch.setenv('OOZIE_HOST', oozie_host)
-
     responses.add(
         responses.GET,
         'http://oozie-host/oozie/v2/jobs?timezone=EST&offset=1&jobtype=coordinator&len=50&filter=name%3Dreportify-rdb-landing-pages-daily-rollup',
@@ -69,51 +27,4 @@ def test_by_name_coordinator(flow_name, coordinators_body, oozie_host, monkeypat
         status=200,
         match_querystring=True,
     )
-    actual = by_name(flow_name, form=Coordinator)
-    assert actual == [
-        CoordinatorObject(
-            '0140935-160503113641936-oozie-oozi-C',
-            'reportify-rdb-landing-pages-daily-rollup',
-            'oozie',
-            'RUNNING',
-            'Wed, 04 May 2016 14:37:00 EST',
-            'Mon, 04 May 2116 14:34:00 EST',
-            'Sat, 07 May 2016 12:37:00 EST'
-        ),
-        CoordinatorObject(
-            '0137532-160413133521481-oozie-oozi-C',
-            'reportify-rdb-landing-pages-daily-rollup',
-            'oozie',
-            'KILLED',
-            'Mon, 02 May 2016 15:37:00 EST',
-            'Sat, 02 May 2116 09:49:00 EST',
-            'Wed, 04 May 2016 15:37:00 EST'
-        ),
-        CoordinatorObject(
-            '0132000-160413133521481-oozie-oozi-C',
-            'reportify-rdb-landing-pages-daily-rollup',
-            'oozie',
-            'KILLED',
-            'Thu, 28 Apr 2016 08:37:00 EST',
-            'Tue, 28 Apr 2116 08:33:00 EST',
-            'Mon, 02 May 2016 10:37:00 EST'
-        ),
-        CoordinatorObject(
-            '0131036-160413133456556-oozie-oozi-C',
-            'reportify-rdb-landing-pages-daily-rollup',
-            'oozie',
-            'KILLED',
-            'Wed, 27 Apr 2016 15:37:00 EST',
-            'Mon, 27 Apr 2116 15:54:00 EST',
-            'Thu, 28 Apr 2016 15:37:00 EST'
-        ),
-        CoordinatorObject(
-            '0120789-160413133521481-oozie-oozi-C',
-            'reportify-rdb-landing-pages-daily-rollup',
-            'oozie',
-            'KILLED',
-            'Thu, 21 Apr 2016 12:37:00 EST',
-            'Tue, 21 Apr 2116 11:45:00 EST',
-            'Wed, 27 Apr 2016 16:37:00 EST'
-        ),
-    ]
+    assert by_name(flow_name, form=Coordinator) == coordinators
